8b0b0fa7ac564bddc50a8c03996c47dea60eea57
Familia Martinez <jumar98@hotmail.com>
2017-05-07 11:17:36 -0500

    Index admin botones

--- app/views/Admin/index.php
+++ app/views/Admin/index.php
@@ -5,6 +5,14 @@
                 <div class="intro-message">
                     <h1 class="red">Bienvenido Admin</h1>
                     <hr class="intro-divider">
+                    <ul class="list-inline intro-social-buttons">
+                        <li>
+                            <a href="<?php echo URL.'Admin/nuevo_partido'; ?>" class="btn btn-default btn-lg"><i class="fa fa-twitter"></i> <span class="network-name">Nuevo partido</span></a>
+                        </li>
+                        <li>
+                            <a href="<?php echo URL.'Jugador/estadisticas'; ?>" class="btn btn-default btn-lg"><i class="fa fa-github"></i> <span class="network-name">Editar jugador</span></a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
